8c1723ed34733233d00b6c0d205e6d71e261cb60
Przemek625 <przemek625@gmail.com>
2018-06-19 12:54:55 +0200

    Add User validation

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -21,13 +21,18 @@ namespace DatingApp.API.Controllers
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
-        {
-            // validate request
+        {   
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if(await _repo.UserExists(userForRegisterDto.Username)){
-                return BadRequest("Username is already taken.");
+
+                ModelState.AddModelError("Username", "Username already exists.");
+                // return BadRequest("Username is already taken.");
             }
+
+            // validate request
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
 
             var userToCreate = new User
             {
