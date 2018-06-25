437e8315bdb9100a59615bf86f38c2f752ee5b85
Przemek625 <przemek625@gmail.com>
2018-06-25 14:27:51 +0200

    Add mappers

--- DatingApp.API/Controllers/UserController.cs
+++ DatingApp.API/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace DatingApp.API.Controllers
 {   
@@ -11,9 +14,12 @@ namespace DatingApp.API.Controllers
     public class UsersController: Controller
     {   
         private readonly IDatingRepository _repo;
-        public UsersController(IDatingRepository repo)
-        {
-            _repo = repo;
+
+        private readonly IMapper _mapper;
+        public UsersController(IDatingRepository repo, IMapper mapper)
+        {   
+            _mapper = mapper;
+            _repo = repo;   
         }
 
         [HttpGet]
@@ -21,7 +27,9 @@ namespace DatingApp.API.Controllers
         {
             var users = await _repo.GetUsers();
 
-            return Ok(users);
+            var userToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            return Ok(userToReturn);
         }
 
         [HttpGet("{id}")]
@@ -29,7 +37,9 @@ namespace DatingApp.API.Controllers
         {      
             var user = await _repo.GetUser(id);
 
-            return Ok(user);
+            var userToReturn = _mapper.Map<UserForDetailDto>(user);
+
+            return Ok(userToReturn);
         }
     }
 }
