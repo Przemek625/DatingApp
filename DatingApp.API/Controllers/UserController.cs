c2db8b9732475ccaff247921b7524c6435f6e535
Przemek625 <przemek625@gmail.com>
2018-06-28 10:38:58 +0200

    Add update user endpoint

--- DatingApp.API/Controllers/UserController.cs
+++ DatingApp.API/Controllers/UserController.cs
@@ -5,21 +5,23 @@ using DatingApp.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Security.Claims;
+using System;
 
 namespace DatingApp.API.Controllers
-{   
+{
     [Authorize]
     [ApiController]
     [Route("api/[controller]")]
-    public class UsersController: Controller
-    {   
+    public class UsersController : Controller
+    {
         private readonly IDatingRepository _repo;
 
         private readonly IMapper _mapper;
         public UsersController(IDatingRepository repo, IMapper mapper)
-        {   
+        {
             _mapper = mapper;
-            _repo = repo;   
+            _repo = repo;
         }
 
         [HttpGet]
@@ -33,13 +35,42 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetUser(int id) 
-        {      
+        public async Task<IActionResult> GetUser(int id)
+        {
             var user = await _repo.GetUser(id);
 
             var userToReturn = _mapper.Map<UserForDetailDto>(user);
 
             return Ok(userToReturn);
+        }
+
+        // api/users/1 PUT
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] UserForUpdateDto userForUpdateDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // this gives id of the current User
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            
+            Console.Write(currentUserId);
+
+            var userFromRepo = await _repo.GetUser(id);
+
+            if (userFromRepo == null)
+                return NotFound($"Could not find user with and ID of {id}");
+
+            if (currentUserId != userFromRepo.Id)
+                return Unauthorized();
+
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repo.saveAll())
+                return NoContent();
+            
+            throw new Exception($"Updating user {id} failed on save");
+
         }
     }
 }
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -34,6 +34,8 @@ namespace DatingApp.API.Helpers
                 }); ;
 
             CreateMap<Photo, PhotosForDetailedDto>();
+
+            CreateMap<UserForUpdateDto, User>();
         }
 
     }
